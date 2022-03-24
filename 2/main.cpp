95be80953b9d547e51cd1f797109f8246b95bd6c
IsaiahWasTaken <98197804+IsaiahWasTaken@users.noreply.github.com>
2022-03-24 00:08:04 -0700

    Update main.cpp

--- 2/main.cpp
+++ 2/main.cpp
@@ -4,11 +4,28 @@
 #include <string>
 #include "Utility.h"
 #include "Car.h"
+using namespace std;
 
 int main() {
     auto integers = randomIntegers();
     std::cout << "Initial: " << integers << "\n";
 
-    // TODO: make the sequence a bitonic sequence
-    std::cout << "Bitonic: " << "TODO" << "\n";
+
+    size_t total = integers.size();
+    size_t half = integers.size() /2;
+    vector<int> piece1(integers.begin() + half, integers.end());
+    vector<int> piece2(integers.begin(), integers.begin() + half);
+
+    sort(piece1.begin(), piece1.end(), greater<int>());
+    sort(piece2.begin(), piece2.end());
+
+
+
+   vector<int> bitonicList(total);
+   bitonicList.insert(bitonicList.end(),piece1.begin(), piece2.end());
+
+
+
+
+    std::cout << "Bitonic: " << bitonicList << "\n";
 }
