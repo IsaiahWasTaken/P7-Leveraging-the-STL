88ac7b675587815bfea14bc0f9bc2b6fd5384926
IsaiahWasTaken <98197804+IsaiahWasTaken@users.noreply.github.com>
2022-03-24 19:51:09 -0700

    Update main.cpp

--- 3/main.cpp
+++ 3/main.cpp
@@ -5,12 +5,24 @@
 #include "Utility.h"
 #include "Car.h"
 
+
 int main() {
     auto cars = randomCars();
     std::cout << "Cars: " << cars << "\n";
 
-    // TODO: print the average price and price range
-    std::cout << "The average price is: $" << "TODO" << "\n";
+    double sum = accumulate(cars.begin(), cars.end(), 0);
+    double avg  = ((sum)/cars.size());
 
-    std::cout << "The range is: $" << "TODO" << "\n";
+    double min = 0;
+    double max = cars[0].price();
+    for(Car& x : cars) {
+        if (x.price() > max){max = x.price();}
+        if(x.price() < min){ min = x.price();}
+    }
+    double range  = (max - min);
+    
+    std::cout << "The average price is: $" << avg << "\n";
+
+    std::cout << "The range is: $" << range << "\n";
 }
+
